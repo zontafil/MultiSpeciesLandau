65259bf9ddeada36e954cb85bc4c0e54c8650f68
Filippo Zonta <zontafil@gmail.com>
2022-04-20 15:13:15 +0300

    Add delete sim script

---
+++ deleteSim.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if [ -z $1 ]; then
+    echo "Usage: saveSim.sh examples/LABEL.cpp"
+    exit
+fi
+
+SIMNAME=${1%.cpp}
+SIMNAME="${SIMNAME##*/}"
+
+rm examples/$SIMNAME.cpp
+rm -rf plots/$SIMNAME.avi
+rm -rf plots/png/${SIMNAME}*.png
+rm -rf plots/eps/${SIMNAME}*.eps
+rm -rf savedData/$SIMNAME
