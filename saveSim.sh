c2e85cbc1a16f374828888e48513bcc1412f7fc2
Filippo Zonta <zontafil@gmail.com>
2022-06-03 12:27:08 +0300

    Fix backup script plots

--- saveSim.sh
+++ saveSim.sh
@@ -16,23 +16,11 @@ cp main.cpp examples/$OUTFILE.cpp
 
 cp out/out.avi plots/$OUTFILE/$OUTFILE.avi
 
-cp out/EnergySpecies.png plots/$OUTFILE/png/Energy.png
-cp out/EnergyError.png plots/$OUTFILE/png/EnergySpecies.png
-cp out/P.png plots/$OUTFILE/png/P.png
-cp out/Vaxes.png plots/$OUTFILE/eps/Vaxes.png
-cp out/PerrNorm.png plots/$OUTFILE/png/PerrNorm.png
-cp out/TemperatureSpecies.png plots/$OUTFILE/png/TemperatureSpecies.png
-cp out/TemperatureSpeciesAxes.png plots/$OUTFILE/png/TemperatureSpeciesAxes.png
-
-cp out/EnergySpecies.eps plots/$OUTFILE/eps/Energy.eps
-cp out/EnergyError.eps plots/$OUTFILE/eps/EnergySpecies.eps
-cp out/P.eps plots/$OUTFILE/eps/P.eps
-cp out/Vaxes.eps plots/$OUTFILE/eps/Vaxes.eps
-cp out/PerrNorm.eps plots/$OUTFILE/png/PerrNorm.eps
-cp out/TemperatureSpecies.eps plots/$OUTFILE/eps/TemperatureSpecies.eps
-cp out/TemperatureSpeciesAxes.eps plots/$OUTFILE/eps/TemperatureSpeciesAxes.eps
+cp out/*.png plots/$OUTFILE/png/
+cp out/*.eps plots/$OUTFILE/eps/
 
 mkdir -p savedData/$OUTFILE
 cp out/data/*.txt savedData/$OUTFILE/
 
+# save frames every 10th
 cp $(printf '%s\n' out/data/*?????.png | awk 'NR%10 == 1') plots/$OUTFILE/frames/
